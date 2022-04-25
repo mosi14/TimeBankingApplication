67c296185979b8159ed25bc14fa8e9ff548a8ad0
Sarmad Raees Khan s294560 <s294560@studenti.polito.it>
2022-04-25 11:19:29 +0500

    layouts added for showprofile and editprofile fragments

--- app/src/main/java/it/polito/mad3/MainActivity.kt
+++ app/src/main/java/it/polito/mad3/MainActivity.kt
@@ -2,10 +2,56 @@ package it.polito.mad3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle : ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            it.isChecked = true
+
+            when(it.itemId){
+                R.id.nav_show_profile -> replaceFragment(ShowProfileFragment(),it.title.toString())
+                R.id.nav_edit_profile -> replaceFragment(EditProfileFragment(),it.title.toString())
+                R.id.nav_ts_details -> replaceFragment(TimeSlotDetailsFragment(),it.title.toString())
+                R.id.nav_ts_edit -> replaceFragment(TimeSlotEditFragment(),it.title.toString())
+                R.id.nav_ts_list -> replaceFragment(TimeSlotListFragment(),it.title.toString())
+                }
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment, title: String,){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
